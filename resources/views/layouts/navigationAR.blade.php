a86fef6afc7fc1bfa222e9a251b2a5fd37943077
Ahmed Samy <ahmedsamyy1998@gmail.com>
2023-10-29 05:20:08 +0200

    Editing Header

--- resources/views/layouts/navigationAR.blade.php
+++ resources/views/layouts/navigationAR.blade.php
@@ -1,17 +1,20 @@
 <div class="page-wrapper">
-    <header class="main-header main-header--one  clearfix">
+    <header class="main-header main-header--one  clearfix sticky-top">
         <div class="main-header--one__wrapper">
             <div class="main-header--one__bottom clearfix">
-                <div class="auto-container">
-                    <div class="main-header--one__bottom-inner flex-row-reverse">
+                <div class="auto-container d-lg-flex justify-content-center" style="padding-left: 260px;">
+                    <div class="main-header--one__bottom-inner row">
                         <nav class="main-menu main-menu--1">
                             <div class="main-menu__inner">
                                 <a href="#" class="mobile-nav__toggler"><i class="fa fa-bars"></i></a>
                                 <div class="stricky-one-logo">
                                     <div class="logo">
+                                        
                                         <a href="/home/ar">
-                                            <img class="light-logo" src="{{ asset('images/logos/logo-1.png') }}"
+                                            <img class="dark-logo" src="{{ asset('images/logos/logo-1.png') }}"
                                                 alt="">
+                                            {{-- <img class="light-logo" src="{{ asset('images/logos/logo-1.png') }}"
+                                                alt=""> --}}
                                         </a>
                                     </div>
                                 </div>
@@ -19,33 +22,43 @@
                                     <ul class="main-menu__list">
 
 
-                                        <li>
+                                        <li class="col-2">
                                             <a href="/home/ar">
                                                 الصفحة الرئيسية
                                             </a>
                                         </li>
-                                        <li><a href="/about/ar">
+                                        <li class="col-1"><a href="/about/ar">
                                                 من نحن
                                             </a></li>
-                                        <li>
+                                        <li class="col">
                                             <a href="/facilities/ar">
                                                 الخدمات
                                             </a>
                                         </li>
-                                        <li>
+                                        <li class=" col-3 d-none d-lg-block">
+                                            <div class="main-header--one__bottom-middel">
+                                                <div class="logo" >
+                                                    <a href="/home/ar">
+                                                        <img class="dark-logo" src="{{ asset('images/logos/logo-1.png') }}" alt="">
+                                                        <img class="light-logo" src="{{ asset('images/logos/logo-2.png') }}" alt="">
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li class="col">
                                             <a href="/shop/ar">
                                                 المنتجات
                                             </a>
                                         </li>
-                                        <li>
+                                        <li class="col">
                                             <a href="/blog/ar">
                                                 المقالات
                                             </a>
                                         </li>
-                                        <li><a href="/contact/ar">
+                                        <li class="col-2"><a href="/contact/ar">
                                                 تواصل معنا
                                             </a></li>
-                                        <li> <button id="toggleLanguage" class="d-none d-lg-block"> english <img
+                                        <li class="col"> <button id="toggleLanguage" class="d-none d-lg-block"><img
                                                     class="px-1"
                                                     src="{{ asset('images/backgrounds/united-kingdom.png') }}"
                                                     alt="" /></button>
@@ -55,14 +68,14 @@
                             </div>
                         </nav>
 
-                        <div class="main-header--one__bottom-middel">
+                        {{-- <div class="main-header--one__bottom-middel">
                             <div class="logo" style="padding-right: 200px">
                                 <a href="/home/ar">
                                     <img class="dark-logo" src="{{ asset('images/logos/logo-1.png') }}" alt="">
                                     <img class="light-logo" src="{{ asset('images/logos/logo-2.png') }}" alt="">
                                 </a>
                             </div>
-                        </div>
+                        </div> --}}
 
                         {{-- <div class="main-header--one__bottom-right clearfix">
                             <div class="search-cart">
@@ -91,7 +104,7 @@
     </header>
 
 
-    <div class="stricky-header stricked-menu main-menu">
+    <div class="stricky-header stricked-menu main-menu d-flex justify-content-center" style="padding-left: 260px;">
         <div class="sticky-header__content">
 
         </div><!-- /.sticky-header__content -->
--- resources/views/layouts/navigationEN.blade.php
+++ resources/views/layouts/navigationEN.blade.php
@@ -1,11 +1,91 @@
-<div class="page-wrapper">
-    <header class="main-header main-header--one  clearfix">
-        <div class="main-header--one__wrapper">
+ <div class="page-wrapper">
+    <header class="main-header main-header--one  clearfix" >
+        <div class="main-header--one__wrapper" >
             <div class="main-header--one__bottom clearfix">
-                <div class="auto-container">
-                    <div class="main-header--one__bottom-inner flex-row-reverse">
-                        <nav class="main-menu main-menu--1">
+                <div class="auto-container d-lg-flex justify-content-center " >
+                    <div class="main-header--one__bottom-inner row ">
+                        <nav class="main-menu main-menu--1" style="right: 15px">
                             <div class="main-menu__inner">
+                                <a href="#" class="mobile-nav__toggler"><i class="fa fa-bars"></i></a>
+                                <div class="stricky-one-logo">
+                                    <div class="logo">
+                                        <a href="/home">
+                                            <img class="dark-logo" src="{{ asset('images/logos/logo-1.png') }}"
+                                                alt="">
+                                            {{-- <img class="light-logo" src="{{ asset('images/logos/logo-2.png') }}"
+                                                alt=""> --}}
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="main-header--one__bottom-left">
+                                    <ul class="main-menu__list">
+
+
+                                        <li class="col">
+                                            <a href="/home">Home</a>
+                                        </li>
+                                        <li class="col"><a href="about">About</a></li>
+                                        <li class="col">
+                                            <a href="facilities">Facilities</a>
+                                        </li>
+                                        <li class=" col-3 d-none d-lg-block">
+                                            <div class="main-header--one__bottom-middel">
+                                                <div class="logo">
+                                                    <a href="/home">
+                                                        <img class="dark-logo"
+                                                            src="{{ asset('images/logos/logo-1.png') }}" alt="">
+                                                        <img class="light-logo"
+                                                            src="{{ asset('images/logos/logo-2.png') }}" alt="">
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li class="col">
+                                            <a href="shop">Products</a>
+                                        </li>
+                                        <li class="col">
+                                            <a href="blog">Blog</a>
+                                        </li>
+                                        <li class="col"><a href="contact">Contact</a></li>
+                                        <li class="col"> <button style="color: white" id="toggleLanguage"
+                                                class="d-none d-lg-block"><img class="px-5"
+                                                    src="{{ asset('images/backgrounds/egypt.png') }}"
+                                                    alt="" /></button>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </nav>
+                        <div class="main-header--one__bottom-middel col-5 order-4 d-block d-lg-none">
+                            <div class="logo">
+                                <a href="/home">
+                                    <img class="dark-logo" src="{{ asset('images/logos/logo-1.png') }}" alt="">
+                                    <img class="light-logo" src="{{ asset('images/logos/logo-2.png') }}" alt="">
+                                </a>
+                            </div>
+                        </div> 
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+
+
+
+
+
+
+
+
+
+ {{-- <header class="  clearfix" >
+        <div class="" >
+            <div class=" clearfix" >
+                <div class="auto-container" >
+                    <div class="" >
+                        <nav class="" >
+                            <div class="">
                                 <a href="#" class="mobile-nav__toggler"><i class="fa fa-bars"></i></a>
                                 <div class="stricky-one-logo">
                                     <div class="logo">
@@ -21,59 +101,59 @@
                                     <ul class="main-menu__list">
 
 
-                                        <li>
+                                        <li class="col">
                                             <a href="/home">Home</a>
                                         </li>
-                                        <li><a href="about">About</a></li>
-                                        <li>
+                                        <li class="col"><a href="about">About</a></li>
+                                        <li class="col">
                                             <a href="facilities">Facilities</a>
                                         </li>
-                                        <li>
+                                        <li class=" col-3 d-none d-lg-block">
+                                            <div class="main-header--one__bottom-middel">
+                                                <div class="logo">
+                                                    <a href="/home">
+                                                        <img class="dark-logo"
+                                                            src="{{ asset('images/logos/logo-1.png') }}" alt="">
+                                                        <img class="light-logo"
+                                                            src="{{ asset('images/logos/logo-2.png') }}" alt="">
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li class="col">
                                             <a href="shop">Products</a>
                                         </li>
-                                        <li>
+                                        <li class="col">
                                             <a href="blog">Blog</a>
                                         </li>
-                                        <li><a href="contact">Contact</a></li>
-                                        <li> <button id="toggleLanguage" class="d-none d-lg-block"><img class="px-2"
+                                        <li class="col"><a href="contact">Contact</a></li>
+                                        <li class="col"> <button style="color: white" id="toggleLanguage"
+                                                class="d-none d-lg-block"><img class="px-2"
                                                     src="{{ asset('images/backgrounds/egypt.png') }}"
-                                                    alt="" />Arabic</button>
+                                                    alt="" /></button>
                                         </li>
                                     </ul>
                                 </div>
                             </div>
                         </nav>
-                        <div class="main-header--one__bottom-middel">
+                        <div class="col-5 order-4 d-block d-lg-none">
                             <div class="logo">
                                 <a href="/home">
                                     <img class="dark-logo" src="{{ asset('images/logos/logo-1.png') }}" alt="">
                                     <img class="light-logo" src="{{ asset('images/logos/logo-2.png') }}" alt="">
                                 </a>
                             </div>
-                        </div>
-                        {{-- <div class="main-header--one__bottom-right clearfix">
-                            <div class="search-cart">
-                                <a href="#" class="search search-toggler"><span
-                                        class="icon-magnifying-glass"></span></a>
-                                <a href="#" class="cart mini-cart__toggler"><span
-                                        class="icon-shopping-cart"></span></a>
-                            </div>
-                            <div class="contact-box">
-                                <div class="icon">
-                                    <span class="icon-phone-call-2"></span>
-                                </div>
-                                <div class="text">
-                                    <p>Call Anytime</p>
-                                    <a href="tel:123456789">666 888 0000</a>
-                                </div>
-                            </div>
-                        </div> --}}
+                        </div> 
                     </div>
                 </div>
             </div>
         </div>
-    </header>
-    <div class="stricky-header stricked-menu main-menu">
+    </header>  --}}
+
+
+
+    
+    <div class="stricky-header stricked-menu main-menu d-flex justify-content-center">
         <div class="sticky-header__content">
         </div><!-- /.sticky-header__content -->
     </div><!-- /.stricky-header -->
