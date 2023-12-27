56e95185fc73ae87a069613707462c8f3eaf0286
box-mkm <mohamedkhaledx12@gmail.com>
2023-12-27 14:00:50 +0200

    mail

--- resources/views/contactAR.blade.php
+++ resources/views/contactAR.blade.php
@@ -45,41 +45,48 @@
             <!--Start Contact Page Right-->
             <div class="col-xl-8 col-lg-8">
                 <div class="contact-page__right">
-                    <form action="https://layerdrops.com/agriox/assets/inc/sendemail.php"
-                        class="comment-one__form contact-form-validated" novalidate="novalidate">
+                    <form action="/mail" method="POST" class="comment-one__form contact-form-validated"
+                        novalidate="novalidate">
+                        @csrf
                         <div class="row">
                             <div class="col-xl-6 col-lg-6">
                                 <div class="comment-form__input-box">
-                                    <input type="text" placeholder="الأسم" name="name">
+                                    <input type="text" placeholder="الاسم" name="name" id="name" required>
                                 </div>
                             </div>
                             <div class="col-xl-6 col-lg-6">
                                 <div class="comment-form__input-box">
-                                    <input type="email" placeholder="الايميل" name="email">
+                                    <input type="email" placeholder="الأيميل" name="email"id="email" required>
                                 </div>
                             </div>
                         </div>
                         <div class="row">
                             <div class="col-xl-6 col-lg-6">
                                 <div class="comment-form__input-box">
-                                    <input type="text" placeholder="رقم الهاتف" name="phone">
+                                    <input type="text" placeholder="رقم الهاتف" name="phone"id="phone" required>
                                 </div>
                             </div>
                             <div class="col-xl-6 col-lg-6">
                                 <div class="comment-form__input-box">
-                                    <input type="email" placeholder="الموضوع" name="Subject">
+                                    <input type="text" placeholder="الموضوع" name="subject"id="subject" required>
                                 </div>
                             </div>
                         </div>
                         <div class="row">
                             <div class="col-xl-12 col-lg-12">
                                 <div class="comment-form__input-box">
-                                    <textarea name="message" placeholder="محتوي الرساله"></textarea>
+                                    <textarea name="message" placeholder="أكتب رسالتك"></textarea>
                                 </div>
-                                <button type="submit" class="thm-btn comment-form__btn">ارسل رسالتك</button>
+                                <button type="submit" class="thm-btn comment-form__btn">Send a message</button>
                             </div>
                         </div>
                     </form>
+                    @error('email')
+                        <div class="alert alert-danger alert-dissmisable fade show" role="alert"> {{ $message }}
+                            <button type="button" class="close" data-dissmiss="alert" aria-label="close">
+                                <span aria-hidden="true">$times;</span></button>
+                        </div>
+                    @enderror
                 </div>
             </div>
             <!--End Contact Page Right-->
